5a4ba0c2bcaa0e2832817ad518a4991bc5f6b97b
Aiden Johnson <arjohnsonn12@gmail.com>
2024-07-03 02:38:51 -0500

    Initial Commit

---
+++ Jarvie A Day/Jarvie_A_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jarvie_A_DayApp.swift
+//  Jarvie A Day
+//
+//  Created by Aiden Johnson on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Jarvie_A_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
